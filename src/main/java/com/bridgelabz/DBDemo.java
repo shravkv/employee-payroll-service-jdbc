62fcea753b07b9f726d8a83e9a8a2b97dde4cc28
shravkv <sravan.k.vodnala@gmail.com>
2022-07-23 21:20:26 +0530

    Added: Database connected to java successfully

--- src/main/java/com/bridgelabz/DBDemo.java
+++ src/main/java/com/bridgelabz/DBDemo.java
@@ -1,11 +1,25 @@
 package com.bridgelabz;
 
 import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DBDemo {
-
     public static void main(String[] args) throws SQLException {
-
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String passWord = "July@135";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded!");
+            System.out.println("Connecting to database :" + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, passWord);
+            System.out.println("Connection is successful!!!!" + connection);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        } finally {
+            connection.close();
+        }
     }
-
 }
